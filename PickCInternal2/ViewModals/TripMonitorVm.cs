68ed60437666f140c7762004c84f2532e269eaf9
Shruthi.T <tshruthi77@gmail.com>
2017-12-05 12:13:41 +0530

    my first commit on pickc

---
+++ PickCInternal2/ViewModals/TripMonitorVm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Master.Contract;
+
+namespace PickC.Internal2.ViewModals
+{
+    public class TripMonitorVm
+    {
+        public Address address { get; set; }
+
+        public string title { get; set; }
+    }
+
+    public class Address
+    {
+        public string address { get; set; }
+        public string lat { get; set; }
+        public string lng { get; set; }
+    }
+
+
+    public class DriverMonitorVm
+    {
+        public List<DriverAttachmentListStatus> driverList { get; set; }
+        public List<TripMonitorVm> tripMonitorVmList { get; set; }
+    }
+    //public class DriverAttachmentListStatus
+    //{
+    //    public string DriverId{ get; set; }
+    //    public string DriverName{ get; set; }
+    //    public string MobileNo{ get; set; }
+    //    public string VehicleNo { get; set; }
+    //    public string Status { get; set; }
+    //    //public string Status { get; set; }
+
+    //}
+}
