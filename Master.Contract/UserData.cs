0133519be7afe0e0b23686a463035a5ef35d53a6
Shruthi.T <tshruthi77@gmail.com>
2017-12-11 18:16:25 +0530

    userapp

---
+++ Master.Contract/UserData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master.Contract
+{
+    public class UserData
+    {
+        public DateTime dateFrom { get; set; }
+        public DateTime dateto { get; set; }
+    }
+}
